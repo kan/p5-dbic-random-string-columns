384856c31346df023da6941d031bc324a4fe37de
tomi-ru <tomi-ru@d0d07461-0603-4401-acd4-de1884942a52>
2008-07-12 14:16:21 +0000

    mk_classdataでデフォルト値を指定するとクラスのデータになってしまうので他のテーブルの同名のフィールドもランダムになってしまう
    git-svn-id: http://svn.coderepos.org/share/lang/perl/DBIx-Class-RandomStringColumns/trunk@15707 d0d07461-0603-4401-acd4-de1884942a52

--- lib/DBIx/Class/RandomStringColumns.pm
+++ lib/DBIx/Class/RandomStringColumns.pm
@@ -11,7 +11,7 @@ use String::Random;
 our $LENGTH = 32;
 our $SALT   = '[A-Za-z0-9]';
 
-__PACKAGE__->mk_classdata( 'rs_definition' => {} );
+__PACKAGE__->mk_classdata( 'rs_definition' );
 
 sub random_string_columns {
     my $self = shift;
@@ -38,7 +38,7 @@ sub random_string_columns {
 
 sub insert {
     my $self = shift;
-    for my $column ( keys( %{ $self->rs_definition } ) ) {
+    for my $column ( keys( %{ $self->rs_definition || {} } ) ) {
         $self->store_column( $column, $self->get_random_string($column) )
           if $self->has_column($column) && !$self->get_column($column);
     }
